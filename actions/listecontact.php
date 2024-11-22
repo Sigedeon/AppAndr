0ed3502a3987578dc11422f7eb119ed9c38637fe
SigedeonDev <gedeonsimeonkimbungu@gmail.com>
2024-11-22 16:31:54 +0100

    ok

--- actions/listecontact.php
+++ actions/listecontact.php
@@ -3,46 +3,20 @@ header('Content-Type: application/json; charset=utf-8');
 include_once('../actions/config.php');
 $json = json_decode(file_get_contents('php://input'), true);
 
-$id = $json['id'];
-$nom = $json['nom'];
-$adresse = $json['adresse'];
-$phone = $json['phone'];
-$decision = $json['decision'];
-$remarque = $json['remarque'];
 $id_user = $json['id_user'];
-$date_save = $json['date_save'];
 
 try {
     // Préparer et exécuter la requête pour récupérer tous les utilisateurs
     $getAllUsers = $bdd->prepare('SELECT * FROM contacts WHERE id_user = ?');
     $getAllUsers->execute($id_user);
 
-    /* $totalAC = $bdd->prepare("SELECT * FROM contacts WHERE decision = 'AC', id_user = ? ");
-    $totalAC->execute($id_user);
 
-    $totalRC = $bdd->prepare("SELECT * FROM contacts WHERE decision = 'RC', id_user = ? ");
-    $totalRC->execute($id_user);
-
-    $totalRDV = $bdd->prepare("SELECT * FROM contacts WHERE decision = 'RDV', id_user = ? ");
-    $totalRDV->execute($id_user);
-
-    $ac = $totalAC->rowCount();
-
-    $rc = $totalRC->rowCount();
-
-    $rdv = $totalRDV->rowCount(); */
-
-
-    // Vérifier si des utilisateurs existent
     if ($getAllUsers->rowCount() > 0) {
-        $users = $getAllUsers->fetchAll(PDO::FETCH_ASSOC); // Récupérer tous les utilisateurs sous forme de tableau associatif
-
-        // Construire la réponse
+        $users = $getAllUsers->fetchAll(PDO::FETCH_ASSOC);
+        
         $result["success"] = true;
         $result["data"] = $users;
-        /* $result["totalac"] = $ac;
-        $result["totalrc"] = $rc;
-        $result["totalrdv"] = $rdv; */
+        
     } else {
         $result["success"] = false;
         $result["error"] = "Aucun enregistrement trouvé";
