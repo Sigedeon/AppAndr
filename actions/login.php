54c7e72b7ec186dc176ec6e95fff9e4d01a98ca5
SigedeonDev <gedeonsimeonkimbungu@gmail.com>
2024-11-22 05:28:39 +0100

    condition

--- actions/login.php
+++ actions/login.php
@@ -9,9 +9,26 @@ if (isset($json['email']) && isset($json['mdp'])) {
     $email = htmlspecialchars($json['email']);
     $mdp = htmlspecialchars($json['mdp']);
 
+
+    $totalAC = $bdd->prepare('SELECT * FROM contacts WHERE decision = AC');
+    $totalAC->execute();
+    
+    $totalRC = $bdd->prepare('SELECT * FROM contacts WHERE decision = RC');
+    $totalRC->execute();
+
+    $totalRDV = $bdd->prepare('SELECT * FROM contacts WHERE decision = RDV');
+    $totalRDV->execute();
+
+    $ac = $totalAC->rowCount();
+
+    $rc = $totalRC->rowCount();
+
+    $rdv = $totalRDV->rowCount();
+
     // Préparer la requête pour rechercher l'utilisateur par email
     $getUser = $bdd->prepare('SELECT * FROM user WHERE email = ?');
     $getUser->execute(array($email));
+    
 
     if ($getUser->rowCount() > 0) {
         $user = $getUser->fetch();
@@ -26,6 +43,12 @@ if (isset($json['email']) && isset($json['mdp'])) {
             $result["prenom"] = $user['prenom'];
             $result["phone"] = $user['phone'];
             $result["equipe"] = $user['equipe'];
+
+            $result["totalac"] = $ac;
+            $result["totalrc"] = $rc;
+            $result["totalrdv"] = $rdv;
+
+
             $result["message"] = "Connexion réussie";
         } else {
             // Mot de passe incorrect
